996f33f6099c3697dde246283bad5675aac56e95
ChangminYi <jerry9803@gmail.com>
2018-05-26 17:04:20 +0900

    enum TEAM, PIECE added, legalMove method added

--- src/chess/ChessBoard.java
+++ src/chess/ChessBoard.java
@@ -1,8 +1,19 @@
 package chess;
 
-public class ChessBoard {
+/**
+ * @author ¿Ã√¢πŒ
+ * Class about chess board, and moving pieces
+ */
+public class ChessBoard extends Tile{
+	/**
+	 * Board is two-dimensional Tile Object array
+	 */
 	Tile[][] board;
 	
+	/**
+	 * ChessBoard
+	 * constructor
+	 */
 	public ChessBoard() {
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board.length; j++) {
@@ -15,6 +26,101 @@ public class ChessBoard {
 				}
 			}
 		}
+		
+		//initializing BLACK team
+		for(int i = 0; i <= 1; i++) {
+			for(int j = 3; j <= 10; j++) {
+				board[i][j].onPiece = true;
+				board[i][j].team = TEAM.BLACK;
+			}
+		}
+		//initializing WHITE team
+		for(int i = 12; i <= 13; i++) {
+			for(int j = 3; j <= 10; j++) {
+				board[i][j].onPiece = true;
+				board[i][j].team = TEAM.WHITE;
+			}
+		}
+		//initializing RED team
+		for(int i = 3; i <= 10; i++) {
+			for(int j = 0; j <= 1; j++) {
+				board[i][j].onPiece = true;
+				board[i][j].team = TEAM.RED;
+			}
+		}
+		//initializing GREEN team
+		for(int i = 3; i <= 10; i++) {
+			for(int j = 12; j <= 13; j++) {
+				board[i][j].onPiece = true;
+				board[i][j].team = TEAM.GREEN;
+			}
+		}
 	}
-	
+
+	/**
+	 * legalMove
+	 * @param x: to-move x coordinate
+	 * @param y: to-move y coordinate
+	 * @param team: team parameter
+	 * @param piece: piece parameter
+	 * @return if move is qualified, return true. else, return false.
+	 */
+	public boolean legalMove(int x, int y, TEAM team, PIECE piece) {
+		if(board[x][y].active) {
+			if(piece == PIECE.PAWN) {	//pawn's move is much fucking than others.
+				switch(team) {
+				case BLACK:
+					if(x) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				case WHITE:
+					if(x) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				case RED:
+					if(x) {
+							return true;
+						}
+						else {
+							return false;
+						}
+				case GREEN:
+					if(x) {
+							return true;
+						}
+						else {
+							return false;
+						}
+				default:
+						break;
+				}
+			}
+			else {	//other's move
+				switch(piece){
+				case KNIGHT:
+					break;
+				case ROOK:
+					break;
+				case BISHOP:
+					break;
+				case QUEEN:
+					break;
+				case KING:
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		else {	//moving to inactive tile
+			System.out.println("illegal move");
+			return false;
+		}
+	}
 }
--- src/chess/Tile.java
+++ src/chess/Tile.java
@@ -1,13 +1,29 @@
 package chess;
 
+/**
+ * @author ¿Ã√¢πŒ
+ * class about single tile
+ */
 public class Tile {
+	/**
+	 * enum about team and piece
+	 * if team is BLACK or White, pawn moves vertically
+	 * if team is RED or GREEN, pawn moves horizontally
+	 */
+	public static enum TEAM{
+		BLACK, WHITE, RED, GREEN
+	}
+	public static enum PIECE{
+		PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+	}
 	boolean active;
-	boolean atkPossible;
 	boolean onPiece;
+	TEAM team;
+	PIECE piece;
 	
 	public Tile(boolean active){
 		this.active = active;
-		this.atkPossible = false;
 		this.onPiece = false;
 	}
+	public Tile() {}
 }
